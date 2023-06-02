3dc9aadf44dc907187b973c801ace6a1ba8fb7ac
Mahedi <632901@student.inholland.nl>
2023-06-02 15:14:38 +0200

    added

--- app.py
+++ app.py
@@ -2,12 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from streamlit_option_menu import option_menu
-import tensorflow as tf
-from sklearn.metrics import confusion_matrix
 import streamlit as st
 from PIL import Image
 import tensorflow as tf
 from tensorflow import keras
+
 
 st.title('Sickle Cell detection')
 
@@ -19,3 +18,41 @@ with st.sidebar:
         options = ["Home","Projects ", "Contact"],
     
     )
+model = tf.keras.models.load_model("model.h5")
+def preprocess_image(image):
+    img = image
+    img = img.resize((299, 299))
+    img_array = np.array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = keras.applications.inception_v3.preprocess_input(img_array)
+    return img_array
+def get_class_label(class_index):
+    class_labels = ['Phase1', 'Phase2', 'Phase3', 'Phase4', 'Phase5']
+    return class_labels[class_index]
+
+def classify_image(image):
+    # Preprocess the image
+    img = preprocess_image(image)
+
+    # Make prediction using the loaded model
+    prediction = model.predict(img)
+    class_index = np.argmax(prediction)
+    class_label = get_class_label(class_index)
+    percentage_predictions = [round(float(p) * 100, 2) for p in prediction[0]]
+
+    return class_label, percentage_predictions
+def main():
+    st.title('Sickle Cell Image Classification')
+    uploaded_file = st.file_uploader('Upload an image', type=['jpg', 'jpeg', 'png'])
+
+    if uploaded_file is not None:
+        # Display the uploaded image
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
+
+        # Perform classification on the uploaded image
+        class_label = classify_image(image)
+        st.write('Predicted Class:', class_label)
+
+if __name__ == '__main__':
+    main()
