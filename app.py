d8113651d9333c9749570e0712bd4db4b5d27291
Mahedi <632901@student.inholland.nl>
2023-06-02 23:09:36 +0200

    added bar chart

--- app.py
+++ app.py
@@ -19,13 +19,16 @@ with st.sidebar:
     
     )
 model = tf.keras.models.load_model("model.h5")
+
 def preprocess_image(image):
-    img = image
-    img = img.resize((299, 299))
+    img = image.resize((299, 299))
+    img = img.convert('RGB')  # Convert image to RGB format
     img_array = np.array(img)
     img_array = np.expand_dims(img_array, axis=0)
-    img_array = keras.applications.inception_v3.preprocess_input(img_array)
+    img_array = tf.keras.applications.inception_v3.preprocess_input(img_array)
     return img_array
+
+
 def get_class_label(class_index):
     class_labels = ['Phase1', 'Phase2', 'Phase3', 'Phase4', 'Phase5']
     return class_labels[class_index]
@@ -40,7 +43,18 @@ def classify_image(image):
     class_label = get_class_label(class_index)
     percentage_predictions = [round(float(p) * 100, 2) for p in prediction[0]]
 
-    return class_label, percentage_predictions
+    # Create a pandas DataFrame for visualization
+    data = pd.DataFrame({
+        'Class': [get_class_label(i) for i in range(len(percentage_predictions))],
+        'Percentage': percentage_predictions
+    })
+
+    # Visualize the percentages as horizontal bars
+    st.bar_chart(data.set_index('Class'))
+
+    return class_label
+
+
 def main():
     st.title('Sickle Cell Image Classification')
     uploaded_file = st.file_uploader('Upload an image', type=['jpg', 'jpeg', 'png'])
