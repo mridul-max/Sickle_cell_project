49c6070dd1952eb5974eb24bd418b3aa518b0d66
Mahedi <632901@student.inholland.nl>
2023-06-12 12:11:42 +0200

    added average prediction

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from streamlit_option_menu import option_menu
-import streamlit as st
 from PIL import Image
 import tensorflow as tf
-from tensorflow import keras
+import cv2
+from streamlit_option_menu import option_menu
 
 
-st.title('Sickle Cell detection')
+st.title('Sickle Cell Image Classification')
 
 #Lets add a sidebar menu
 
@@ -18,6 +17,7 @@ with st.sidebar:
         options = ["Home","Projects ", "Contact"],
     
     )
+# Load the trained model
 model = tf.keras.models.load_model("model.h5")
 
 def preprocess_image(image):
@@ -27,7 +27,6 @@ def preprocess_image(image):
     img_array = np.expand_dims(img_array, axis=0)
     img_array = tf.keras.applications.inception_v3.preprocess_input(img_array)
     return img_array
-
 
 def get_class_label(class_index):
     class_labels = ['Phase1', 'Phase2', 'Phase3', 'Phase4', 'Phase5']
@@ -52,21 +51,44 @@ def classify_image(image):
     # Visualize the percentages as horizontal bars
     st.bar_chart(data.set_index('Class'))
 
-    return class_label
-
+    return class_label, percentage_predictions
 
 def main():
-    st.title('Sickle Cell Image Classification')
-    uploaded_file = st.file_uploader('Upload an image', type=['jpg', 'jpeg', 'png'])
+    uploaded_files = st.file_uploader('Upload multiple images', type=['jpg', 'jpeg', 'png'], accept_multiple_files=True)
 
-    if uploaded_file is not None:
-        # Display the uploaded image
-        image = Image.open(uploaded_file)
-        st.image(image, caption='Uploaded Image', use_column_width=True)
+    if uploaded_files:
+        # Store the uploaded images
+        images = []
+        for uploaded_file in uploaded_files:
+            # Display the uploaded image
+            image = Image.open(uploaded_file)
+            st.image(image, caption='Uploaded Image', use_column_width=True)
+            images.append(image)
 
-        # Perform classification on the uploaded image
-        class_label = classify_image(image)
-        st.write('Predicted Class:', class_label)
+        if st.button('Classify Images'):
+            all_predictions = []
+            all_percentage_predictions = []
+            for image in images:
+                # Perform classification on each uploaded image
+                class_label, percentage_predictions = classify_image(image)
+                st.write('Predicted Class:', class_label)
+                st.write('Percentage Predictions:', percentage_predictions)
+                all_predictions.append(class_label)
+                all_percentage_predictions.append(percentage_predictions)
+                st.write('---')  # Add a separator between images
+
+            if all_percentage_predictions:
+                # Calculate average percentage predictions
+                average_prediction = np.mean(all_percentage_predictions, axis=0)
+
+                # Create a pandas DataFrame for visualization
+                data = pd.DataFrame({
+                    'Class': [get_class_label(i) for i in range(len(average_prediction))],
+                    'Percentage': average_prediction
+                })
+
+                # Visualize the average percentages as horizontal bars
+                st.bar_chart(data.set_index('Class'))
 
 if __name__ == '__main__':
     main()
